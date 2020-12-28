a7baa0e3d1b9424b42b738c361279e280d92c510
anis95 <abaki.anis@gmail.com>
2020-12-28 04:58:27 +0100

    fourth commit test technique

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import BrandDetails from "./components/pages/BrandDetails";
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Route
-          exact={true}
-          path="/brand/:id"
-          render={() => {
-            return (
-              <div>
-                <BrandDetails />
-              </div>
-            );
-          }}
-        />
-      </BrowserRouter>
-    );
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <Route
+        exact={true}
+        path="/brand/:id"
+        render={() => {
+          return (
+            <div>
+              <BrandDetails />
+            </div>
+          );
+        }}
+      />
+    </BrowserRouter>
+  );
 }
 
 export default App;
